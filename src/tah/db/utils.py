bed23d36356bb2c71c013e558123b78ccf35b4a0
tbongkim03 <tbongkim03@gmial.com>
2024-07-25 15:34:57 +0900

    test

--- src/tah/db/utils.py
+++ src/tah/db/utils.py
@@ -1,9 +1,18 @@
 import pandas as pd
 
-def top():
+def read_data():
     df = pd.read_parquet('./data/parquet')
-    fdf = df[df['dt'] == '2024-07-14']
+    return df
+
+def top(cnt, dt):
+    df = read_data()
+    fdf = df[df['dt'] == dt]
     sdf = fdf.sort_values(by='cnt', ascending=False).head(10)
     ddf = sdf.drop(columns=['dt'])
     r = ddf.to_string(index=False)
     return r
+
+def count(query):
+    df = read_data()
+    fdf = df[df['dt'].str.contains(query)]
+    cnt = fdf['cnt'].sum
