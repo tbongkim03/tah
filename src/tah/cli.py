02666df4ed9252c82cde7a57b29808ce1cbc8fd9
tbongkim03 <tbongkim03@gmial.com>
2024-07-25 14:05:36 +0900

    cli change

--- src/tah/cli.py
+++ src/tah/cli.py
@@ -1,3 +1,4 @@
+import argparse
 
 def hello_msg():
     return "hello"
@@ -5,4 +6,20 @@ def hello_msg():
 def cmd11():
     msg = hello_msg()
     print(msg)
-    
+   
+    parser = argparse.ArgumentParser(
+            prog="ProgramName",
+            description='What the program does',
+            epilog='Text at the bottom of help')
+
+    parser.add_argument('filename') # positional argument
+    parser.add_argument('-c', '--count') # option that takes a value
+    parser.add_argument('-v', '--verbose', action='store_true') # on/off flag
+
+    args = parser.parse_args()
+    print(args.filename, args.count, args.verbose)
+
+    if True:
+        print("verbose ON")
+    else:
+        print("fff")
