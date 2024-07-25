7e6e26c9de8ecb403269e60387f1647a8cc851b1
tbongkim03 <tbongkim03@gmial.com>
2024-07-25 14:32:30 +0900

    print_help

--- src/tah/cli.py
+++ src/tah/cli.py
@@ -22,14 +22,14 @@ def cmd11():
 
     if args.scount:
         print(f"-s -> {args.scount}")
+        # TODO 명령어 카운트
     elif args.top:
         print(f"-t -> {args.top}")
         if args.dt:
             print(f"-d -> {args.dt}")
+            # TODO 특정 날짜의 명령어 TOP N
         else:
-            #TODO
+            #TODO 에러나 안내메시지
             pass
-    elif None:
-        print("help 명령어 추가")
     else:
-        pass
+        parser.print_help()
